4daad8268d0132d00baf416b4dfb8b533f8ef748
Meddebsofien <sofien.meddeb@esprit.tn>
2024-03-08 00:53:34 +0100

    reste validation&&search

--- Frontend/src/frontoffice/layout/offer/Offer.jsx
+++ Frontend/src/frontoffice/layout/offer/Offer.jsx
@@ -43,44 +43,41 @@ const Offer = ({
         </Card.Body>
       </Card>  
   */}
-
-      <div
-        className="w-full md:w-[16rem] 2xl:w-[18rem] h-[14rem] md:h-[14rem] bg-white flex flex-col justify-between shadow-lg 
-                rounded-md px-3 py-5 "
-        key={Id}
+      <Link
+        className="bg-white hover:bg-gray-500  text-black"
+        to={`details/${Id}`}
       >
-        <div className="flex gap-3">
-          <img src={imgSrc} className="w-14 h-14" />
+        <div
+          className="w-full md:w-[16rem] 2xl:w-[18rem] h-[14rem] md:h-[14rem] bg-white flex flex-col justify-between shadow-lg 
+                rounded-md px-3 py-5 "
+          key={Id}
+        >
+          <div className="flex gap-3">
+            <img src={imgSrc} className="w-14 h-14" />
 
-          <div className="">
-            <p className="text-lg font-semibold truncate">{title}</p>
-            <span className="flex gap-2 items-center">
-              <GoLocation className="text-slate-900 text-sm" />
-              {location}
+            <div className="">
+              <p className="text-lg font-semibold truncate">{title}</p>
+              <span className="flex gap-2 items-center">
+                <GoLocation className="text-slate-900 text-sm" />
+                {location}
+              </span>
+            </div>
+          </div>
+          <div className="py-3">
+            <p className="text-sm truncate">{description}</p>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <p className="bg-[#1d4fd826] text-[#1d4fd8] py-0.5 px-1.5 rounded font-semibold text-sm">
+              {type}
+            </p>
+            <span className="text-gray-500 text-sm">
+              {moment(createdAt).fromNow()}
             </span>
           </div>
+          <div className="flex justify-start pt-3 "></div>
         </div>
-        <div className="py-3">
-          <p className="text-sm truncate">{description}</p>
-        </div>
-
-        <div className="flex items-center justify-between">
-          <p className="bg-[#1d4fd826] text-[#1d4fd8] py-0.5 px-1.5 rounded font-semibold text-sm">
-            {type}
-          </p>
-          <span className="text-gray-500 text-sm">
-            {moment(createdAt).fromNow()}
-          </span>
-        </div>
-        <div className="flex justify-start pt-3 ">
-          <Button
-            className="button bg-red-600  text-white w-20 h-10 hover:bg-gray-600 "
-            onClick={() => onDetails(Id)}
-          >
-            Details
-          </Button>
-        </div>
-      </div>
+      </Link>
     </>
   );
 };
