2c29059b24b8cb050f8be90daa4f33174a0b6371
Emna <emna.felfel@esprit.tn>
2024-05-02 13:08:44 +0200

    navbar

--- Frontend/src/frontoffice/pages/Navbar.jsx
+++ Frontend/src/frontoffice/pages/Navbar.jsx
@@ -1,204 +1,275 @@
-import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import {
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBCard,
-  MDBCardBody,
-  MDBTypography,
-  MDBBtn,
-  MDBIcon,
-  MDBTextArea,
-} from "mdb-react-ui-kit";
-import io from "socket.io-client";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import EditIcon from "@mui/icons-material/Edit";
 
-export default function ChatRoom() {
-  const [studentUsers, setStudentUsers] = useState([]);
-  const [messages, setMessages] = useState([]);
-  const [messageInput, setMessageInput] = useState("");
-  const [socket, setSocket] = useState(null);
+function Navbar() {
+  const [idc, setIdc] = useState("");
+  const [twofa, settwofa] = useState("");
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      const [header, payload, signature] = token.split(".");
+      const decodedPayload = JSON.parse(atob(payload));
+      setIdc(decodedPayload.id);
+      settwofa(decodedPayload.twofaEnabled);
+      console.log("idc", decodedPayload.id);
+      console.log("twofa", decodedPayload.twofaEnabled);
+    } else {
+      console.log("Token non trouvé dans localStorage");
+    }
+  }, []);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/signin");
+  };
+  const handleProfileClick = () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const [, payload] = token.split(".");
+      const decodedPayload = JSON.parse(atob(payload));
+      const userId = decodedPayload.id; // Supposons que l'ID utilisateur est stocké dans le token
+      if (userId) {
+        navigate(`/Student/updateprofile/${userId}`); // Redirection vers la page de mise à jour du profil avec l'ID utilisateur
+      } else {
+        console.log("ID utilisateur non trouvé dans le token");
+      }
+    } else {
+      console.log("Token non trouvé dans localStorage");
+      // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
+      navigate("/login");
+    }
+  };
 
   useEffect(() => {
-    fetchStudentUsers();
-    initializeSocket();
-    fetchAllMessages();
-  }, []);
-
-  const fetchStudentUsers = async () => {
-    try {
-      const response = await fetch("http://localhost:3700/users/users/Student");
-      if (response.ok) {
-        const data = await response.json();
-        setStudentUsers(data);
+    const select = (el, all = false) => {
+      el = el.trim();
+      if (all) {
+        return [...document.querySelectorAll(el)];
       } else {
-        console.error("Failed to fetch student users");
+        return document.querySelector(el);
       }
-    } catch (error) {
-      console.error("Error fetching student users:", error);
-    }
-  };
-
-  const fetchSenderInfo = async (senderId) => {
-    try {
-      const response = await fetch(`http://localhost:3700/users/getutilisateur/${senderId}`);
-      if (response.ok) {
-        const data = await response.json();
-        return data;
-      } else {
-        console.error("Failed to fetch sender information");
-        return null;
-      }
-    } catch (error) {
-      console.error("Error fetching sender information:", error);
-      return null;
-    }
-  };
-
-  const fetchAllMessages = async () => {
-    try {
-      const response = await fetch("http://localhost:3700/messages/all");
-      if (response.ok) {
-        const messages = await response.json();
-        const messagesWithSenderInfo = await Promise.all(messages.map(async (message) => {
-          const senderInfo = await fetchSenderInfo(message.senderId._id);
-          return { ...message, senderInfo };
-        }));
-        setMessages(messagesWithSenderInfo);
-      } else {
-        console.error("Failed to fetch messages");
-      }
-    } catch (error) {
-      console.error("Error fetching messages:", error);
-    }
-  };
-
-  const initializeSocket = () => {
-    const newSocket = io("http://localhost:3001");
-    newSocket.on("connect", () => {
-      console.log("Connected to server");
-    });
-    newSocket.on("message", (message) => {
-      setMessages((prevMessages) => [...prevMessages, message]);
-    });
-    setSocket(newSocket);
-  };
-  const { userId } = useParams();
-
-  const handleSendMessage = async () => {
-    if (!socket || !messageInput.trim()) return;
-
-    const message = {
-      senderId: userId, // Use the userId obtained from the URL params
-      content: messageInput.trim(),
-      time: new Date().toLocaleTimeString(),
     };
 
-    try {
-      const response = await fetch("http://localhost:3700/messages/send", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(message),
-      });
-
-      if (response.ok) {
-        console.log("Message sent successfully");
-        socket.emit("message", message);
-        setMessages((prevMessages) => [...prevMessages, message]);
-        setMessageInput("");
-      } else {
-        console.error("Failed to send message");
+    const on = (type, el, listener, all = false) => {
+      let selectEl = select(el, all);
+      if (selectEl) {
+        if (all) {
+          selectEl.forEach((e) => e.addEventListener(type, listener));
+        } else {
+          selectEl.addEventListener(type, listener);
+        }
       }
-    } catch (error) {
-      console.error("Error sending message:", error);
+    };
+
+    const onscroll = (el, listener) => {
+      el.addEventListener("scroll", listener);
+    };
+
+    const navbarlinksActive = () => {
+      let position = window.scrollY + 200;
+      let navbarlinks = select("#navbar .scrollto", true);
+      navbarlinks.forEach((navbarlink) => {
+        if (!navbarlink.hash) return;
+        let section = select(navbarlink.hash);
+        if (!section) return;
+        if (
+          position >= section.offsetTop &&
+          position <= section.offsetTop + section.offsetHeight
+        ) {
+          navbarlink.classList.add("active");
+        } else {
+          navbarlink.classList.remove("active");
+        }
+      });
+    };
+
+    window.addEventListener("load", navbarlinksActive);
+    onscroll(document, navbarlinksActive);
+
+    const scrollto = (el) => {
+      let header = select("#header");
+      let offset = header.offsetHeight;
+
+      if (!header.classList.contains("header-scrolled")) {
+        offset -= 16;
+      }
+
+      let elementPos = select(el).offsetTop;
+      window.scrollTo({
+        top: elementPos - offset,
+        behavior: "smooth",
+      });
+    };
+
+    const headerScrolled = () => {
+      let selectHeader = select("#header");
+      if (window.scrollY > 100) {
+        selectHeader.classList.add("header-scrolled");
+      } else {
+        selectHeader.classList.remove("header-scrolled");
+      }
+    };
+
+    window.addEventListener("load", headerScrolled);
+    onscroll(document, headerScrolled);
+
+    const toggleBacktotop = () => {
+      let backtotop = select(".back-to-top");
+      if (backtotop) {
+        if (window.scrollY > 100) {
+          backtotop.classList.add("active");
+        } else {
+          backtotop.classList.remove("active");
+        }
+      }
+    };
+
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+
+    const mobileNavToggle = (e) => {
+      let navbar = select("#navbar");
+      navbar.classList.toggle("navbar-mobile");
+      e.currentTarget.classList.toggle("bi-list");
+      e.currentTarget.classList.toggle("bi-x");
+    };
+
+    on("click", ".mobile-nav-toggle", mobileNavToggle);
+
+    const mobileNavDropdownsActivate = (e) => {
+      let navbar = select("#navbar");
+      if (navbar.classList.contains("navbar-mobile")) {
+        e.preventDefault();
+        e.currentTarget.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    };
+
+    on("click", ".navbar .dropdown > a", mobileNavDropdownsActivate, true);
+
+    const scrollToElement = (e) => {
+      if (select(e.currentTarget.hash)) {
+        e.preventDefault();
+        let navbar = select("#navbar");
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
+        scrollto(e.currentTarget.hash);
+      }
+    };
+
+    on("click", ".scrollto", scrollToElement, true);
+
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash);
+      }
     }
+
+    return () => {
+      // Clean up event listeners when the component unmounts
+      window.removeEventListener("load", navbarlinksActive);
+      window.removeEventListener("scroll", navbarlinksActive);
+      window.removeEventListener("load", headerScrolled);
+      window.removeEventListener("scroll", headerScrolled);
+      window.removeEventListener("load", toggleBacktotop);
+      window.removeEventListener("scroll", toggleBacktotop);
+      document.querySelectorAll(".mobile-nav-toggle").forEach((toggle) => {
+        toggle.removeEventListener("click", mobileNavToggle);
+      });
+      document.querySelectorAll(".navbar .dropdown > a").forEach((link) => {
+        link.removeEventListener("click", mobileNavDropdownsActivate);
+      });
+      document.querySelectorAll(".scrollto").forEach((link) => {
+        link.removeEventListener("click", scrollToElement);
+      });
+    };
+  }, []);
+
+  const handleEnable2FA = () => {
+    // Redirect to the 2FA setup page for the specific user
+    navigate(`/Student/twoFA/${idc}`);
   };
 
-
-
   return (
-    <MDBContainer fluid className="py-5" style={{ backgroundColor: "#eee" }}>
-      <MDBRow>
-        <MDBCol md="4">
-          <h5 className="font-weight-bold mb-3 text-center text-lg-start">
-            Users who are students
-          </h5>
-          <MDBCard>
-            <MDBCardBody>
-              <MDBTypography listUnStyled className="mb-0">
-                {studentUsers.map((user) => (
-                  <li
-                    key={user._id}
-                    className="p-2 border-bottom"
-                    style={{ backgroundColor: "#eee" }}
-                  >
-                    <a href="#!" className="d-flex justify-content-between">
-                      <div className="d-flex flex-row">
-                        <img
-                          src={
-                            user.photo.startsWith("http")
-                              ? user.photo
-                              : `http://localhost:3700/${user.photo}`
-                          }
-                          alt="avatar"
-                          className="rounded-circle d-flex align-self-center me-3 shadow-1-strong"
-                          width="60"
-                        />
-                        <div className="pt-1">
-                          <p className="fw-bold mb-0">{user.nom}</p>
-                          <p className="small text-muted">{user.specialite}</p>
-                        </div>
-                      </div>
-                      <div className="pt-1">
-                        <p className="small text-muted mb-1">Just now</p>
-                        <span className="badge bg-danger float-end">1</span>
-                      </div>
-                    </a>
-                  </li>
-                ))}
-              </MDBTypography>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
+    <header id="header" className="fixed-top bg-white">
+      <div className="container d-flex  align-items-center justify-content-between">
+        <Link href="home" className="w-25 h-10">
+          <img
+            src="/src/assets/img/logo.png"
+            alt=""
+            className="img-fluid"
+            style={{ width: "150px", height: "50px" }}
+          />
+        </Link>
 
-        <MDBCol md="8">
-          <MDBTypography listUnStyled>
-            <ul>
-              {messages.map((message, index) => (
-                <li key={index}>
-                  <MDBCard
-                    className={`mb-2 ${
-                      message.senderInfo ? "bg-info" : "bg-light"
-                    }`}
-                  >
-                    <MDBCardBody>
-                      <p className="fw-bold mb-0">
-                        {message.senderInfo ? message.senderInfo.nom : "Sender"}
-                      </p>
-                      <p className="small text-muted mb-1">{message.time}</p>
-                      <p className="mb-0">{message.content}</p>
-                    </MDBCardBody>
-                  </MDBCard>
-                </li>
-              ))}
-            </ul>
-            <li className="bg-white mb-3">
-              <MDBTextArea
-                label="Message"
-                id="textAreaExample"
-                rows={4}
-                value={messageInput}
-                onChange={(e) => setMessageInput(e.target.value)}
-              />
+        <nav id="navbar" className="navbar">
+          <ul>
+            <li>
+              <Link
+                className="nav-link scrollto active text-black"
+                to={`/Student`}
+              >
+                Home
+              </Link>
             </li>
-            <MDBBtn color="info" rounded className="float-end" onClick={handleSendMessage}>
-              Send
-            </MDBBtn>
-          </MDBTypography>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+            <li>
+              <Link className="nav-link scrollto text-black">Campanies</Link>
+            </li>
+
+            <li>
+              <Link className="nav-link scrollto text-black" to={``}>
+                My Applications
+              </Link>
+            </li>
+
+            <li className="dropdown">
+              <Link href="#">
+                <span className="text-black">Profil</span>{" "}
+                <i className="bi bi-chevron-down"></i>
+              </Link>
+              <ul>
+                <li>
+                  <button
+                    //onClick={handleEditProfile}
+                    style={{ display: "flex", alignItems: "center" }}
+                    onClick={handleProfileClick}
+                  >
+                    <EditIcon style={{ marginRight: "5px" }} /> Edit profil
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={handleEnable2FA}
+                    style={{ display: "flex", alignItems: "center" }}
+                    disabled={twofa}
+                  >
+                    <EditIcon style={{ marginRight: "5px" }} /> Enable 2FA
+                  </button>
+                </li>
+
+                <li>
+                  <button
+                    onClick={handleLogout}
+                    style={{ display: "flex", alignItems: "center" }}
+                  >
+                    <ExitToAppIcon style={{ marginRight: "5px" }} /> logOut
+                  </button>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <i className="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+      </div>
+    </header>
   );
 }
+
+export default Navbar;
